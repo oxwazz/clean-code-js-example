ba735b05d998b38e9f04c64b03109b2aef6a7a94
oxwazz <muhammad.rahmahalim@gmail.com>
2022-04-19 07:18:18 +0700

    fix: delete params

--- 1-Variables/3-Avoid Magic Number.js
+++ 1-Variables/3-Avoid Magic Number.js
@@ -9,12 +9,13 @@ function checkKeyboardEvent(event) {
   }
 }
 
-const shuffleCards = (event) => {
+const shuffleCards = () => {
   for (let cardIndex = 0; cardIndex < 52; cardIndex++) {
     // shuffle card;
   }
 };
 
+// GOOD CODE
 // const MILLISECONDS_PER_DAY = 60 * 60 * 24 * 1000; //86400000;
 // setTimeout(() => {
 //   // ...
@@ -27,7 +28,7 @@ const shuffleCards = (event) => {
 //   }
 // }
 
-// const shuffleCards = (event) => {
+// const shuffleCards = () => {
 //   const DECK_SIZE = 52;
 //   for (let cardIndex = 0; cardIndex < DECK_SIZE; cardIndex++) {
 //     // shuffle card;
